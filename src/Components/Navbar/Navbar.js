6ec78445b1831398efd18ecbb7699662893fd7ad
terance-edmonds <teranceedmonds20@gmail.com>
2020-12-02 23:51:31 +0530

    2020-12-03

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -1,10 +1,10 @@
 import React, { useState } from 'react'
-import { Link, animateScroll as scroll } from 'react-scroll';
+import { Link, animateScroll as scroll } from 'react-scroll'
 import { FaTimes , FaBars } from 'react-icons/fa'
 import './Navbar.css'
 
-
 function Navbar() {
+
     const name = "< T.A.Edmonds />";
 
     const [ click , setClick ] = useState(false)
@@ -14,47 +14,108 @@ function Navbar() {
     const toggle_home = () => {
         scroll.scrollToTop();
         setClick(false)
-    }
-    
+     }
+
     return (
         <div className="navbar__container">
+            <div className="navbar__grid-item">
+                <Link><h1 className="navbar__head" onClick={toggle_home}>{name}</h1></Link>
+
+                <div className="navbar__menu-icon" onClick={handleClick}>
+                    {click ? <FaTimes /> : <FaBars />}
+                </div>
+
+            </div>
             
             <div className="navbar__grid-item">
-                <Link> <h2 className="navbar__text" onClick={toggle_home}> {name} </h2> </Link>
-            </div>
 
-            
-            <div className="navbar__menu-icon" onClick={handleClick}>
-                {click ? <FaTimes /> : <FaBars />}
-            </div>
-
-            <div className="navbar__grid-item">
-                <ul className={ click ? 'navbar__links-wrapper navbar__active' : 'navbar__links-wrapper'}>
-                    <Link to='skills' spy={true} smooth={true} offset={-50} duration={1000} className="navbar__link" onClick={closeMobileMenu}>
+                <ul className={click ? "navbar__links-wrapper navbar__active" : "navbar__links-wrapper"}>
+                    <Link to='skills' spy={true} smooth={true} offset={-50} duration={1000} className="navbar__link" onClick={closeMobileMenu} >
                         <li className="navbar__link-item">
-                                    Skills
+                            Skills
                         </li>
                     </Link>
-                    <Link to='carrier' offset={-60} spy={true} smooth={true} duration={1000} className="navbar__link" onClick={closeMobileMenu}>
+                    <Link to='carrier' spy={true} smooth={true} offset={-50} duration={1000} className="navbar__link" onClick={closeMobileMenu}>
                         <li className="navbar__link-item">
-                                Carrier
+                            Carrier
                         </li>
                     </Link>
-                    <Link to='projects' offset={-50} spy={true} smooth={true} duration={1000} className="navbar__link" onClick={closeMobileMenu}>
+                    <Link to='projects' spy={true} smooth={true} offset={-50} duration={1000} className="navbar__link" onClick={closeMobileMenu}>
                         <li className="navbar__link-item">
-                                Projects 
+                            Projects
                         </li>
                     </Link>
-                    <Link to='contact-me' spy={true} smooth={true} duration={1000} className="navbar__link" onClick={closeMobileMenu}>
+                    <Link to='contact-me' spy={true} smooth={true} offset={-50} duration={1000} className="navbar__link" onClick={closeMobileMenu}>
                         <li className="navbar__link-item">
-                                Contact Me 
+                            Contact Me
                         </li>
                     </Link>
                 </ul>
             </div>
-            
         </div>
     )
 }
 
 export default Navbar
+
+
+// import React, { useState } from 'react'
+// import { Link, animateScroll as scroll } from 'react-scroll';
+// import { FaTimes , FaBars } from 'react-icons/fa'
+// import './Navbar.css'
+
+
+// function Navbar() {
+//     const name = "< T.A.Edmonds />";
+
+//     const [ click , setClick ] = useState(false)
+//     const handleClick = () => setClick(!click)
+//     const closeMobileMenu = () => setClick(!click)
+
+//     const toggle_home = () => {
+//         scroll.scrollToTop();
+//         setClick(false)
+//     }
+    
+//     return (
+//         <div className="navbar__container">
+            
+//             <div className="navbar__grid-item">
+//                 <Link> <h2 className="navbar__text" onClick={toggle_home}> {name} </h2> </Link>
+//             </div>
+
+            
+//             <div className="navbar__menu-icon" onClick={handleClick}>
+//                 {click ? <FaTimes /> : <FaBars />}
+//             </div>
+
+//             <div className="navbar__grid-item">
+//                 <ul className={ click ? 'navbar__links-wrapper navbar__active' : 'navbar__links-wrapper'}>
+//                     <Link to='skills' spy={true} smooth={true} offset={-50} duration={1000} className="navbar__link" onClick={closeMobileMenu}>
+//                         <li className="navbar__link-item">
+//                                     Skills
+//                         </li>
+//                     </Link>
+//                     <Link to='carrier' offset={-60} spy={true} smooth={true} duration={1000} className="navbar__link" onClick={closeMobileMenu}>
+//                         <li className="navbar__link-item">
+//                                 Carrier
+//                         </li>
+//                     </Link>
+//                     <Link to='projects' offset={-50} spy={true} smooth={true} duration={1000} className="navbar__link" onClick={closeMobileMenu}>
+//                         <li className="navbar__link-item">
+//                                 Projects 
+//                         </li>
+//                     </Link>
+//                     <Link to='contact-me' spy={true} smooth={true} duration={1000} className="navbar__link" onClick={closeMobileMenu}>
+//                         <li className="navbar__link-item">
+//                                 Contact Me 
+//                         </li>
+//                     </Link>
+//                 </ul>
+//             </div>
+            
+//         </div>
+//     )
+// }
+
+// export default Navbar
